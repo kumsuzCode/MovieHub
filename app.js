5a609d5f29caf8d5d5d6e3082582212f7d402c8e
kumsuzCode <mkumsuz1@gmail.com>
2022-08-27 18:39:51 +1000

    Final route fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const form = document.getElementById("form");
-console.log(window.Location.origin);
+
 if (form) {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     const userInput = document.getElementById("search-bar").value;
     if (!userInput) return;
     localStorage.setItem("input", userInput);
-    window.location.href = `${window.Location.origin}/search.html`;
+    window.location.href = `${window.location.origin}/search.html`;
   });
 }
